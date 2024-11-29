d94e6e99fe3b3ccd791c0cce76c0739a604bf8c3
hqtong <18868195165@163.com>
2024-11-29 15:43:30 +0800

    日常更新

--- 功能测试/相关性检验/create_data.py
+++ 功能测试/相关性检验/create_data.py
@@ -25,24 +25,36 @@ def get_trait():
     df['repo_7'] = df['repo'].shift(7)
     df['trait_x1'] = df.apply(lambda x: x['repo'] - x['repo_7'],axis = 1)
     df = df.dropna(subset=['repo_7'])
-    print(df.head())
-    return df['trait_x1'].values
+    df.loc[:,'日期Date'] = df['day_stap'].apply(lambda x: x.replace('-',''))
+    print(df.head(1))
+    df['日期Date'] =  df['日期Date'].astype('str')
+    df.to_csv('repo_7_test.csv')
+
+    print(df.columns)
+
+    return df
 
 def get_label(ts_code = '000002.SH'):
-    fn = '000001perf.xlsx'
+    fn = '000001_SH.csv'
     infn = os.path.join(Config_base.base_path,'功能测试/相关性检验',fn)
     df = Config_base.read_data(infn)
-    df.to_csv('000001_SH.csv')
-    print(df.head())
+    df['日期Date'] =  df['日期Date'].astype('str')
+    df
+    print(df.head(1))
+    print(df.columns)
 
-
-    # data.to_csv('test.csv',index = 0)
-    # print(data.columns)
-    # print(data)
+    return df
 
 
 if __name__ == '__main__':
-    get_label()
+    df_1 = get_label()
+    df_2 = get_trait()
+    aim_df = df_2.merge(df_1,on = '日期Date')
+    aim_df = aim_df.dropna()
+    # aim_df = pd.DataFrame(aim_df,columns = ['日期Date','repo_7',''])
+    aim_df = pd.DataFrame(aim_df,columns = ['日期Date','trait_x1','收盘Close'])
+    aim_df.to_csv('basic_data.csv')
 
+    # print()
 
 
